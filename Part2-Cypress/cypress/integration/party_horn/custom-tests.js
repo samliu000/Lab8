66437bf2f1304a386f6a9a9873c61e3873f71164
samliu000 <44206874+samliu000@users.noreply.github.com>
2021-02-24 19:35:56 -0800

    Made link

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -1,9 +1,16 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress/index.html');
   });
 
   it('First Test', () => {
     expect(true).to.equal(true);
+  });
+
+  it("Slider changes when volume input changes", () => {
+    cy.get("#volume-number").clear().type('75');
+    cy.get('#volume-slider').then( ($el) => {
+      expect($el).to.have.value(75);
+    });
   });
 });
